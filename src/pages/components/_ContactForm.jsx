e106d7bf37fdad7d450bfadc289873270a993668
alamraimporters <alamraimporters@gmail.com>
2023-10-22 19:36:07 -0400

    upadte

--- src/pages/components/_ContactForm.jsx
+++ src/pages/components/_ContactForm.jsx
@@ -2,12 +2,34 @@
 
 import React, { useState, useRef} from "react";
 import emailjs from '@emailjs/browser';
-import Header from "./_Header";
-import Footer from "./_Footer";
-import { isValidEmail } from "../utilityFuncs/isValidEmail";
 
 const Contact = () => {
 
+  
+function isValidEmail(emailText) {
+  // checks if an emali is valid by checking if it has a dot and if it has an @
+  // return true for a valie email and false for an invalid email
+  let atCount = 0;
+  let dotCount = 0;
+  for (let i = 0; i < emailText.length; i++) {
+    let char = emailText[i];
+    if (char === '@'){
+      atCount++;
+      for (let j = i+1; j < emailText.length; j++){
+          char = emailText[j];
+          if (char === '.'){
+              dotCount++
+          }
+          if (char === '@'){
+              return false;
+          }
+      }
+      break
+    }
+  }
+
+  return !(atCount !== 1 || dotCount === 0)
+};
 
 
   const [formState, setFormState] = useState({});
